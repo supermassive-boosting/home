1d032efdf142e66ae9477f5ac94576639a5c0087
yankikucuk <kendinikertenkelebek@me.com>
2024-08-15 01:08:08 +0300

    update

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>House of Naked Dreams</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">
+      bastıkça artan buton: {{ count }}
+    </button>
+  </div>
+</template>
